174d84ea34299b218143c5b9ff450ff0b33111e5
Radite <joshuameghoo@gmail.com>
2023-12-26 00:09:03 -0500

    Initial Athlete Colletion Script

--- Get_Athletes.py
+++ Get_Athletes.py
@@ -5,10 +5,9 @@ import time
 import os
 
 current_directory = os.getcwd()
-print(current_directory)
 
 # Combine the current directory with the filename
-csv_file_path = os.path.join(current_directory, 'FilteredCountries.xlsx')
+csv_file_path = os.path.join(current_directory, 'FilteredCountries.csv')
 
 # Read the CSV file
 country_codes_df = pd.read_csv(csv_file_path)
@@ -23,7 +22,7 @@ driver = webdriver.Chrome()
 seen_names = set()  # To track seen athlete names
 
 # Function to scrape athlete names and DOBs
-def scrape_athletes(country_code, discipline, gender, seen_names, required_number=10):
+def scrape_athletes(country_code, discipline, gender, seen_names, required_number=5):
     url = f"https://worldathletics.org/athletes-home?countryCode={country_code}&disciplineCode={discipline}&gender={gender}&environment=outdoor"
     driver.get(url)
     time.sleep(5)  # Wait for the JavaScript to load the content
@@ -35,7 +34,6 @@ def scrape_athletes(country_code, discipline, gender, seen_names, required_numbe
     athletes_found = 0
 
     for athlete_element in athlete_elements:
-        print(seen_names)
         if athletes_found >= required_number:
             break  # Stop if we have found the required number of athletes
 
@@ -76,23 +74,62 @@ def scrape_athletes(country_code, discipline, gender, seen_names, required_numbe
 
     return athletes_data
 
+def save_data_to_csv(data, file_path):
+    # Convert data to DataFrame
+    df = pd.DataFrame(data, columns=['CountryCode', 'Nationality', 'Country', 'Discipline', 'Gender', 'Athlete', 'DOB'])
+    if not os.path.isfile(file_path):
+        df.to_csv(file_path, index=False)
+    else:
+        df.to_csv(file_path, mode='a', header=False, index=False)
+
+
+def get_last_saved_point(file_path):
+    if os.path.isfile(file_path):
+        df = pd.read_csv(file_path)
+        if not df.empty:
+            last_row = df.iloc[-1]
+            return last_row['CountryCode'], str(last_row['Discipline']), last_row['Gender']
+    return None, None, None
+
+
+last_country_code, last_discipline, last_gender = get_last_saved_point('athletes.csv')
+
+resume = False
+if last_country_code and last_discipline and last_gender:
+    resume = True
+
+
 data = []
+batch_size = 50  # Adjust as needed
 
 for _, row in country_codes_df.iterrows():
     country_code = row['Codes']
     nationality = row['Nationality']
     country_name = row['Country']
 
+    if resume and country_code < last_country_code:
+        continue  # Skip to the last saved country code
+
     for discipline in disciplines:
+        if resume and discipline < last_discipline:
+            continue  # Skip to the last saved discipline
+
         for gender in ['male', 'female']:
+            if resume and gender < last_gender:
+                continue  # Skip to the last saved gender
+
+            resume = False  # Reset resume flag after reaching resumption point
+
             athletes_data = scrape_athletes(country_code, discipline, gender, seen_names)
             for athlete_name, athlete_dob in athletes_data:
                 data.append([country_code, nationality, country_name, discipline, gender, athlete_name, athlete_dob])
 
+                if len(data) >= batch_size:
+                    save_data_to_csv(data, 'athletes.csv')
+                    data = []  # Reset data after saving
 
-# Close the WebDriver
+# Save any remaining data after the loop
+if data:
+    save_data_to_csv(data, 'athletes.csv')
+
 driver.quit()
-
-# Create a DataFrame and save to CSV
-df = pd.DataFrame(data, columns=['CountryCode', 'Nationality', 'Country', 'Discipline', 'Gender', 'Athlete', 'DOB'])
-df.to_csv('athletes.csv', index=False)
